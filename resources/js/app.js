f49d797cd51b0c03bc42c804c249f53756c8a100
muneeb876781 <muneebto11256@gmail.com>
2024-03-20 11:31:00 +0500

    text editors in product listings

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,18 @@
 import './bootstrap';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ) )
+    .then( editor => {
+        console.log( editor );
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -18,6 +18,8 @@
     <link rel="stylesheet" href="{{ asset('assets/modules/apexcharts/apexcharts.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <script src="https://cdn.tiny.cloud/1/elmma06n570gih5simypugr5mexr6mqv82cnbnodgqcxmpmg/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+
 
 
 </head>
@@ -136,9 +138,24 @@
                                         </div>
                                         <div class="mb-3">
                                             <label for="CategoriesDescription">Categories Description:</label>
-                                            <textarea class="form-control" id="CategoriesDescription" name="CategoriesDescription"
+                                            <textarea class="form-control" id="editor" name="CategoriesDescription"
                                                 placeholder="Enter Product Description"></textarea>
                                         </div>
+                                        <script>
+                                            tinymce.init({
+                                              selector: 'textarea',
+                                              plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+                                              toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                                              tinycomments_mode: 'embedded',
+                                              tinycomments_author: 'Author name',
+                                              height: 250,
+                                              mergetags_list: [
+                                                { value: 'First.Name', title: 'First Name' },
+                                                { value: 'Email', title: 'Email' },
+                                              ],
+                                              ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+                                            });
+                                          </script>
                                     </div>
 
                                 </div>
